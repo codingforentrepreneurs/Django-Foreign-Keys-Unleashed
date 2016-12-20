20abaf906345b185f946b57fcf46432da07f9288
CFE <hello@teamcfe.com>
2016-12-20 15:25:30 -0800

    2 - First Foreign Key Relation

--- src/cars/admin.py
+++ src/cars/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Car
+
+
+admin.site.register(Car)
--- src/cars/models.py
+++ src/cars/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL # 'auth.User'
+
+
+class Car(models.Model):
+    user    = models.ForeignKey(User)
+    name    = models.CharField(max_length=120)
+
+    def __str__(self): # __unicode__
+        return self.name
